9a45297a2d541df5c9f52bd8cdd6d787b83c03cc
John Yuan <jyuan@unc.edu>
2021-10-01 11:27:56 -0400

    update on ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,36 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730458275"
+
 
 # Define your functions below
+def only_evens(xs: list[int],) -> list[int]:
+    """Returns list of even numbers."""
+    new_list: list[int] = []
+    for number in xs:
+        if number % 2 == 0:
+            new_list.append(number)
+        
+    return new_list
+
+
+def sub(xs: list[int], int_one: int, int_two: int) -> list[int]:
+    """Creates a new list."""
+    new_list: list[int] = []
+    i = 0
+    while i < len(xs):
+        x = xs[i]
+        if i >= int_one and i <= (int_two - 1):
+            new_list.append(x)
+        i += 1
+    return new_list
+
+
+def concat(xs: list[int], ys: list[int]) -> list[int]:
+    """Concats two different lists into one."""
+    new_list: list[int] = []
+    for x in xs:
+        new_list.append(x)
+    for y in ys:
+        new_list.append(y)
+    return new_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -3,4 +3,63 @@
 # TODO: Uncomment the below line when ready to write unit tests
 # from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730458275"
+
+from exercises.ex05.utils import only_evens
+from exercises.ex05.utils import concat
+from exercises.ex05.utils import sub
+
+
+def test_even_use_one() -> None:
+    """Test one for even."""
+    assert only_evens([1, 2, 3, 4]) == [2, 4]
+    return None
+
+
+def test_even_use_edge() -> None:
+    """Test two for even."""
+    assert only_evens([]) == []
+    return None
+
+
+def test_even_use_two() -> None:
+    """Test three for even."""
+    assert only_evens([2, 4, 6, 8]) == [2, 4, 6, 8]
+    return None
+
+
+def test_sub_use_one() -> None:
+    """Test 1 for sub."""
+    assert sub([1, 2, 3], 0, 1) == [1]
+    return None
+
+
+def test_sub_use_edge() -> None:
+    """Test 2 for sub."""
+    assert sub([], 0, 1) == []
+    return None
+
+
+def test_sub_use_two() -> None:
+    """Test 3 for sub."""
+    assert sub([0, 2, 4, 5, 6], 0, 4) == [0, 2, 4, 5]
+    return None 
+
+
+def test_concat_use_one() -> None:
+    """Test 1 for concat."""
+    assert concat([1, 2], [3, 4]) == [1, 2, 3, 4]
+  
+    return None
+
+
+def test_concat_use_two() -> None:
+    """Test 2 for concat."""
+    assert concat([1], [2]) == [1, 2]
+    return None
+
+
+def test_concat_use_edge() -> None:
+    """Test 3 for concat."""
+    assert concat([], []) == []
+    return None
